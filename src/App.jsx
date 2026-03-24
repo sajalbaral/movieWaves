ba6ccb3f7d3a212b392e3bb9a396e9b4a4189430
sajalbaral <sajalsbaral@gmail.com>
2026-03-23 20:00:13 -0400

    created a movie card that renders the movie img, rating, language, and release date

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Search from "./components/Search";
 import { useState, useEffect } from "react";
 import Spinner from "./components/Spinner";
+import { MovieCard } from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -75,9 +76,7 @@ const App = () => {
             ) : (
               <ul>
                 {movies.map((movie) => (
-                  <li key={movie.id} className="text-white">
-                    {movie.title}
-                  </li>
+                  <MovieCard key={movie.id} movie={movie} />
                 ))}
               </ul>
             )}
